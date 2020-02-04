e54d9355d16286e8da43a6776d415d4f246e43a9
Oskar Stark <oskarstark@googlemail.com>
2020-02-04 06:27:49 +0100

    Typo

--- src/Model/ModelManager.php
+++ src/Model/ModelManager.php
@@ -279,7 +279,7 @@ class ModelManager implements ModelManagerInterface
             throw new \RunTimeException('Invalid argument, object or null required');
         }
 
-        // the entity is not managed
+        // the document is not managed
         if (!$document || !$this->getDocumentManager($document)->getUnitOfWork()->isInIdentityMap($document)) {
             return;
         }
