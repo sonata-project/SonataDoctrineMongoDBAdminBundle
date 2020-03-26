d13e3b6b4b53cba8213ef244876296a72c693898
Vincent Langlet <vincentlanglet@hotmail.fr>
2020-03-26 23:13:55 +0100

    Remove default SortBy

--- src/Model/ModelManager.php
+++ src/Model/ModelManager.php
@@ -403,8 +403,6 @@ class ModelManager implements ModelManagerInterface
     public function getDefaultSortValues($class)
     {
         return [
-            '_sort_order' => 'ASC',
-            '_sort_by' => $this->getModelIdentifier($class),
             '_page' => 1,
             '_per_page' => 25,
         ];
