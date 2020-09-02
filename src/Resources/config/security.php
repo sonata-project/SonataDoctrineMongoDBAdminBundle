6f289b70cea49cdbca8bcb9ea9b9c3288e92231b
Fran Moreno <franmomu@gmail.com>
2020-09-02 16:28:30 +0200

    Move internal configuration to PHP

---
+++ src/Resources/config/security.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Sonata\DoctrineMongoDBAdminBundle\Util\ObjectAclManipulator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->parameters()
+
+        ->set('sonata.admin.manipulator.acl.object.doctrine_mongodb.class', ObjectAclManipulator::class);
+
+    // Use "param" function for creating references to parameters when dropping support for Symfony 5.1
+    $containerConfigurator->services()
+
+        ->set('sonata.admin.manipulator.acl.object.doctrine_mongodb', '%sonata.admin.manipulator.acl.object.doctrine_mongodb.class%');
+};
