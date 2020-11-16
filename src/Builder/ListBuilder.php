b6c2b3b99c88f0089da996467676106b1d8e2c61
Fran Moreno <franmomu@gmail.com>
2020-11-16 20:19:24 +0100

    Add assertions for ModelManager
    The methods called are not part of ModelManagerInterface.

--- src/Builder/ListBuilder.php
+++ src/Builder/ListBuilder.php
@@ -19,6 +19,7 @@ use Sonata\AdminBundle\Admin\FieldDescriptionCollection;
 use Sonata\AdminBundle\Admin\FieldDescriptionInterface;
 use Sonata\AdminBundle\Builder\ListBuilderInterface;
 use Sonata\AdminBundle\Guesser\TypeGuesserInterface;
+use Sonata\DoctrineMongoDBAdminBundle\Model\ModelManager;
 
 /**
  * @final since sonata-project/doctrine-mongodb-admin-bundle 3.5.
@@ -77,8 +78,12 @@ class ListBuilder implements ListBuilderInterface
 
         $fieldDescription->setAdmin($admin);
 
-        if ($admin->getModelManager()->hasMetadata($admin->getClass())) {
-            [$metadata, $lastPropertyName, $parentAssociationMappings] = $admin->getModelManager()->getParentMetadataForProperty($admin->getClass(), $fieldDescription->getName());
+        $modelManager = $admin->getModelManager();
+
+        \assert($modelManager instanceof ModelManager);
+
+        if ($modelManager->hasMetadata($admin->getClass())) {
+            [$metadata, $lastPropertyName, $parentAssociationMappings] = $modelManager->getParentMetadataForProperty($admin->getClass(), $fieldDescription->getName());
             $fieldDescription->setParentAssociationMappings($parentAssociationMappings);
 
             // set the default field mapping
