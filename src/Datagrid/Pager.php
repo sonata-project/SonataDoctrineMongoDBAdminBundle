9c80a5e5d40faaa885aa59b225eaad7f9e05d3d8
Fran Moreno <franmomu@gmail.com>
2021-01-20 19:16:58 +0100

    Remove Pager::getResults

--- src/Datagrid/Pager.php
+++ src/Datagrid/Pager.php
@@ -90,22 +90,6 @@ class Pager extends BasePager
         return $this->getQuery()->execute();
     }
 
-    /**
-     * NEXT_MAJOR: remove this method.
-     *
-     * @deprecated since sonata-project/doctrine-mongodb-admin-bundle 3.x.
-     */
-    public function getResults(): array
-    {
-        @trigger_error(sprintf(
-            'Method "%s()" is deprecated since sonata-project/doctrine-mongodb-admin-bundle 3.x and will'
-        .' be removed in 4.0. Use "getCurrentPageResults()" instead.',
-            __METHOD__,
-        ), \E_USER_DEPRECATED);
-
-        return $this->getCurrentPageResults();
-    }
-
     public function init(): void
     {
         // NEXT_MAJOR: Remove next line.
