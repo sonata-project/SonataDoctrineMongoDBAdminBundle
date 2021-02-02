f21d023eda2c741dab3bb157d4486b39fb30a92e
Fran Moreno <franmomu@gmail.com>
2021-02-02 21:14:24 +0100

    Introduce ProxyQueryInterface
    This will allow to remove __call method from ProxyQuery.

---
+++ src/Datagrid/ProxyQueryInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\DoctrineMongoDBAdminBundle\Datagrid;
+
+use Doctrine\ODM\MongoDB\Query\Builder;
+use Sonata\AdminBundle\Datagrid\ProxyQueryInterface as BaseProxyQueryInterface;
+
+interface ProxyQueryInterface extends BaseProxyQueryInterface
+{
+    /**
+     * @return Builder
+     */
+    public function getQueryBuilder();
+}
