a2c8526a87760f71ceb32ecb85947fd484e3194e
Fran Moreno <franmomu@gmail.com>
2021-02-21 18:04:02 +0100

    Update FieldDescription tests
    Tests related to BaseFieldDescription have been removed since
    those methods are already tested in sonata-project/admin-bundle.
    
    Also a test has been added for FieldDescription::getValue method.

--- tests/Admin/FieldDescriptionTest.php
+++ tests/Admin/FieldDescriptionTest.php
@@ -14,77 +14,10 @@ declare(strict_types=1);
 namespace Sonata\DoctrineMongoDBAdminBundle\Tests\Admin;
 
 use PHPUnit\Framework\TestCase;
-use Sonata\AdminBundle\Admin\AbstractAdmin;
-use Sonata\AdminBundle\Admin\AdminInterface;
-use Sonata\AdminBundle\Admin\FieldDescriptionInterface;
-use Sonata\AdminBundle\Exception\NoValueException;
 use Sonata\DoctrineMongoDBAdminBundle\Admin\FieldDescription;
 
 class FieldDescriptionTest extends TestCase
 {
-    public function testOptions(): void
-    {
-        $field = new FieldDescription('name');
-        $field->setOptions([
-            'template' => 'foo',
-            'type' => 'bar',
-            'misc' => 'foobar',
-        ]);
-
-        // test method shortcut
-        $this->assertNull($field->getOption('template'));
-        $this->assertNull($field->getOption('type'));
-
-        $this->assertSame('foo', $field->getTemplate());
-        $this->assertSame('bar', $field->getType());
-
-        // test the default value option
-        $this->assertSame('default', $field->getOption('template', 'default'));
-
-        // test the merge options
-        $field->setOption('array', ['key1' => 'val1']);
-        $field->mergeOption('array', ['key1' => 'key_1', 'key2' => 'key_2']);
-
-        $this->assertSame(['key1' => 'key_1', 'key2' => 'key_2'], $field->getOption('array'));
-
-        $field->mergeOption('non_existant', ['key1' => 'key_1', 'key2' => 'key_2']);
-
-        $this->assertSame(['key1' => 'key_1', 'key2' => 'key_2'], $field->getOption('array'));
-
-        $field->mergeOptions(['array' => ['key3' => 'key_3']]);
-
-        $this->assertSame(['key1' => 'key_1', 'key2' => 'key_2', 'key3' => 'key_3'], $field->getOption('array'));
-
-        $field->setOption('integer', 1);
-
-        try {
-            $field->mergeOption('integer', []);
-            $this->fail('no exception raised !!');
-        } catch (\RuntimeException $e) {
-        }
-
-        $field->mergeOptions(['final' => 'test']);
-
-        $expected = [
-          'misc' => 'foobar',
-          'placeholder' => 'short_object_description_placeholder',
-          'link_parameters' => [],
-          'array' => [
-            'key1' => 'key_1',
-            'key2' => 'key_2',
-            'key3' => 'key_3',
-          ],
-          'non_existant' => [
-            'key1' => 'key_1',
-            'key2' => 'key_2',
-          ],
-          'integer' => 1,
-          'final' => 'test',
-        ];
-
-        $this->assertSame($expected, $field->getOptions());
-    }
-
     public function testAssociationMapping(): void
     {
         $field = new FieldDescription(
@@ -111,111 +44,6 @@ class FieldDescriptionTest extends TestCase
         $this->assertSame('integer', $field->getType());
         $this->assertSame('overwritten', $field->getFieldName());
         $this->assertSame('integer', $field->getType());
-    }
-
-    public function testSetName(): void
-    {
-        $field = new FieldDescription('name');
-        $field->setName('New field description name');
-
-        $this->assertSame($field->getName(), 'New field description name');
-    }
-
-    public function testSetNameSetFieldNameToo(): void
-    {
-        $field = new FieldDescription('New field description name');
-
-        $this->assertSame($field->getFieldName(), 'New field description name');
-    }
-
-    /**
-     * NEXT_MAJOR: Remove this test.
-     */
-    public function testSetNameDoesNotSetFieldNameWhenSetBefore(): void
-    {
-        $field = new FieldDescription('name');
-        $field->setFieldName('field name', 'sonata_deprecation_mute');
-        $field->setName('New field description name');
-
-        $this->assertSame($field->getFieldName(), 'field name');
-    }
-
-    public function testGetParent(): void
-    {
-        $adminMock = $this->createMock(AdminInterface::class);
-        $field = new FieldDescription('name');
-        $field->setParent($adminMock);
-
-        $this->assertSame($adminMock, $field->getParent());
-    }
-
-    public function testGetAdmin(): void
-    {
-        $adminMock = $this->createMock(AdminInterface::class);
-        $field = new FieldDescription('name');
-        $field->setAdmin($adminMock);
-
-        $this->assertSame($adminMock, $field->getAdmin());
-    }
-
-    public function testGetAssociationAdmin(): void
-    {
-        $adminMock = $this->createMock(AbstractAdmin::class);
-        $adminMock->expects($this->once())
-            ->method('setParentFieldDescription')
-            ->with($this->isInstanceOf(FieldDescriptionInterface::class));
-
-        $field = new FieldDescription('name');
-        $field->setAssociationAdmin($adminMock);
-
-        $this->assertSame($adminMock, $field->getAssociationAdmin());
-    }
-
-    public function testHasAssociationAdmin(): void
-    {
-        $adminMock = $this->createMock(AbstractAdmin::class);
-        $adminMock->expects($this->once())
-            ->method('setParentFieldDescription')
-            ->with($this->isInstanceOf(FieldDescriptionInterface::class));
-
-        $field = new FieldDescription('name');
-
-        $this->assertFalse($field->hasAssociationAdmin());
-
-        $field->setAssociationAdmin($adminMock);
-
-        $this->assertTrue($field->hasAssociationAdmin());
-    }
-
-    public function testGetValue(): void
-    {
-        $object = new class() {
-            public function myMethod()
-            {
-                return 'myMethodValue';
-            }
-        };
-
-        $field = new FieldDescription('name');
-        $field->setOption('code', 'myMethod');
-
-        $this->assertSame($field->getValue($object), 'myMethodValue');
-    }
-
-    public function testGetValueWhenCannotRetrieve(): void
-    {
-        $object = new class() {
-            public function myMethod()
-            {
-                return 'myMethodValue';
-            }
-        };
-
-        $field = new FieldDescription('name');
-
-        $this->expectException(NoValueException::class);
-
-        $this->assertSame($field->getValue($object), 'myMethodValue');
     }
 
     public function testGetAssociationMapping(): void
@@ -351,5 +179,38 @@ class FieldDescriptionTest extends TestCase
         $field = new FieldDescription('name', [], $fieldMapping);
 
         $this->assertSame($fieldMapping, $field->getFieldMapping());
+    }
+
+    public function testGetParentValue(): void
+    {
+        $parentAssociationMappings = [
+            ['fieldName' => 'parent'],
+        ];
+
+        $field = new FieldDescription('name', [], [], [], $parentAssociationMappings);
+
+        $dummyParent = new class() {
+            public function name(): string
+            {
+                return 'hi';
+            }
+        };
+
+        $dummyChild = new class($dummyParent) {
+            /** @var object */
+            private $parent;
+
+            public function __construct(object $parent)
+            {
+                $this->parent = $parent;
+            }
+
+            public function parent(): object
+            {
+                return $this->parent;
+            }
+        };
+
+        $this->assertSame('hi', $field->getValue($dummyChild));
     }
 }
