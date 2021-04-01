e0957a22b8db0b8dd96e37ea7c2aba9f9e2c7b97
Fran Moreno <franmomu@gmail.com>
2021-04-01 12:03:20 +0200

    Add some asertion in referenced objects

--- tests/App/DataFixtures/MongoDB/BookFixtures.php
+++ tests/App/DataFixtures/MongoDB/BookFixtures.php
@@ -16,14 +16,25 @@ namespace Sonata\DoctrineMongoDBAdminBundle\Tests\App\DataFixtures\MongoDB;
 use Doctrine\Bundle\MongoDBBundle\Fixture\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Sonata\DoctrineMongoDBAdminBundle\Tests\App\Document\Author;
 use Sonata\DoctrineMongoDBAdminBundle\Tests\App\Document\Book;
+use Sonata\DoctrineMongoDBAdminBundle\Tests\App\Document\Category;
 
 final class BookFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $book = new Book('book_id', 'Don Quixote', $this->getReference(AuthorFixtures::AUTHOR));
-        $book->addCategory($this->getReference(CategoryFixtures::CATEGORY));
+        $author = $this->getReference(AuthorFixtures::AUTHOR);
+
+        \assert($author instanceof Author);
+
+        $book = new Book('book_id', 'Don Quixote', $author);
+
+        $category = $this->getReference(CategoryFixtures::CATEGORY);
+
+        \assert($category instanceof Category);
+
+        $book->addCategory($category);
 
         $manager->persist($book);
         $manager->flush();
