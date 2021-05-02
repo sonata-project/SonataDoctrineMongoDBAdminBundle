d3741c7d033bb66986bb3cb493a18b9d094c4a85
Fran Moreno <franmomu@gmail.com>
2021-05-03 00:39:12 +0200

    Add forward compatibility with FilterData
    `FilterData` will be introduced in version 4.0 as type declaration
    of argument 4 of `Filter::apply()`.
    
    This commit adds an upgrade path when using `CallbackFilter` which
    receives a `callable` instance in order to filter.
    
    If this `callable` does not use a type declaration for argument
    4 or uses `array` (current type), it triggers a deprecation.

--- tests/Filter/CallbackFilterTest.php
+++ tests/Filter/CallbackFilterTest.php
@@ -15,9 +15,13 @@ namespace Sonata\DoctrineMongoDBAdminBundle\Tests\Filter;
 
 use Sonata\DoctrineMongoDBAdminBundle\Datagrid\ProxyQuery;
 use Sonata\DoctrineMongoDBAdminBundle\Filter\CallbackFilter;
+use Sonata\DoctrineMongoDBAdminBundle\Tests\Util\CallbackClass;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
 
 class CallbackFilterTest extends FilterWithQueryBuilderTest
 {
+    use ExpectDeprecationTrait;
+
     /**
      * @param mixed $value
      *
@@ -119,5 +123,39 @@ class CallbackFilterTest extends FilterWithQueryBuilderTest
         $this->expectException(\RuntimeException::class);
 
         $filter->apply($builder, 'myValue');
+    }
+
+    /**
+     * NEXT_MAJOR: Remove this test.
+     *
+     * @group legacy
+     *
+     * @dataProvider provideCallables
+     */
+    public function testItThrowsDeprecationWithoutFilterData(callable $callable): void
+    {
+        $proxyQuery = new ProxyQuery($this->getQueryBuilder());
+
+        $filter = new CallbackFilter();
+        $filter->initialize('field_name_test', [
+            'callback' => $callable,
+            'field_name' => 'field_name_test',
+        ]);
+
+        $this->expectDeprecation('Not adding "Sonata\AdminBundle\Filter\Model\FilterData" as type declaration for argument 4 is deprecated since sonata-project/doctrine-orm-admin-bundle 3.x and the argument will be a "Sonata\AdminBundle\Filter\Model\FilterData" instance in version 4.0.');
+
+        $filter->apply($proxyQuery, ['value' => 'myValue']);
+        $this->assertTrue($filter->isActive());
+    }
+
+    /**
+     * @phpstan-return iterable<array{callable}>
+     */
+    public function provideCallables(): iterable
+    {
+        yield 'static class method call' => [[CallbackClass::class, 'staticCallback']];
+        yield 'object method call as array' => [[new CallbackClass(), 'callback']];
+        yield 'invokable class with array type declaration' => [new CallbackClass()];
+        yield 'anonymous function' => [static function ($query, $alias, $field, $data): bool { return true; }];
     }
 }
